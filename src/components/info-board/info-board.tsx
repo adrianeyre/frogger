289d81549c4f646c8427c966c32fa24fb264413d
AvaxArrogant <95660335+AvaxArrogant@users.noreply.github.com>
2023-05-31 02:21:42 -0400

    infoboard

--- src/components/info-board/info-board.tsx
+++ src/components/info-board/info-board.tsx
@@ -16,24 +16,26 @@ const InfoBoard: FC<IInfoBoardProps> = (props: IInfoBoardProps) => {
 	return <div className="info-board" style={ styleInfoBoard() }>
 		<div className="info-board-header">
 			<img src={ playerRight } alt="player" />
-			<span className="header-text">Frogger</span>
+			<span className="header-text">Smol Frogz</span>
 			<img src={playerLeft } alt="player" />
 		</div>
 
 		{ props.gameOver && <div className="game-over-area">
-			<div className="game-over-title">Game Over</div>
+			<div className="game-over-title">Canceled</div>
 			<div className="game-over-text">You scored { props.score }, better luck next time!</div>
 		</div> }
 
 		<div className="info-board-instructions">
 			<ul>
-				<li>You control Frogger, guiding him in four directions with the arrow keys.</li>
-				<li>You must guide Frogger to each of the five empty slots at the top of the screen in order to advance to the next stage.</li>
-				<li>You start at the bottom of the screen beneath five lanes of traffic.</li>
-				<li>You must reach the sidewalk in the middle of the screen without getting hit by any vehicles.</li>
-				<li>You must not fall in the water on the way to one of the slots at the top. Ride the turtles and logs to the top, but don't get pulled off the side of the screen.</li>
-				<li>Beware of diving turtles. If Frogger is on them when they dive, he loses a life.</li>
-				<li>Avoid crocodile mouths, catch flies in the slots for bonus points, and bring the lady frog to your home for more bonus points.</li>
+			    <li>Users must own a Smol Frogz NFT to play.</li>
+				<li>You control your Smol Frogz NFT, by guiding it with the arrow keys.</li>
+				<li>You must guide your Smol Frogz NFT to each of the empty slots up top in order to advance to the next stage.</li>
+				<li>You start at the bottom of the screen beneath five lanes of obstacles.</li>
+				<li>You must reach the sidewalk in the middle of the screen without getting hit.</li>
+				<li>Don't fall in the water along the way. Ride the turtles and logs to the top.</li>
+				<li>Beware of diving turtles. If your Smol Frogz NFT is on them when they dive, he loses a life.</li>
+				<li>Avoid Fud & catch rpepe tokens for bonus points, and bring a lady frogz home tp score additional bonus points.</li>
+				<li>Connect To Continue.</li>
 			</ul>
 		</div>
 
