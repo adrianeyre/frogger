b482143a40fc081276c57419301e1d0ad362c922
AvaxArrogant <95660335+AvaxArrogant@users.noreply.github.com>
2023-05-31 08:06:33 -0400

    Revert "updatres"
    This reverts commit 4837f650b31552c63ecf85cf22f12dbf8f200a3b.

--- src/components/info-board/info-board.tsx
+++ src/components/info-board/info-board.tsx
@@ -25,14 +25,16 @@ const InfoBoard: FC<IInfoBoardProps> = (props: IInfoBoardProps) => {
 			<div className="game-over-text">You scored { props.score }, better luck next time!</div>
 		</div> }
 
-		<div className="info-board-instructions" style={{ textAlign: 'center' }}>
+		<div className="info-board-instructions">
 			<ul>
 			    <li>Users must own a Smol Frogz NFT to play.</li>
-				<li>Control your Smol Frog, by guiding it with the arrow keys.</li>
-				<li>You must guide your frog to each of the empty slots up top in order to advance to the next stage.</li>
-				<li>Don't fall in the water along the way. Ride the whales and logs to the top.</li>
-				<li>Beware of diving whales. If your Smol Frog is on them when they dive, you will lose a life.</li>
-				<li>Avoid Fud & bring a lady frogz home to score bonus points.</li>
+				<li>You control your Smol Frogz NFT, by guiding it with the arrow keys.</li>
+				<li>You must guide your Smol Frogz NFT to each of the empty slots up top in order to advance to the next stage.</li>
+				<li>You start at the bottom of the screen beneath five lanes of obstacles.</li>
+				<li>You must reach the sidewalk in the middle of the screen without getting hit.</li>
+				<li>Don't fall in the water along the way. Ride the turtles and logs to the top.</li>
+				<li>Beware of diving turtles. If your Smol Frogz NFT is on them when they dive, he loses a life.</li>
+				<li>Avoid Fud & catch $rpepe tokens for bonus points, and bring a lady frogz home tp score additional bonus points.</li>
 			</ul>
 		</div>
 
