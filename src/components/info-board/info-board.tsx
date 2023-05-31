e2b789202cbd46f938509894592ef4ef3cdd37f7
AvaxArrogant <95660335+AvaxArrogant@users.noreply.github.com>
2023-05-31 07:51:33 -0400

    Update info-board.tsx

--- src/components/info-board/info-board.tsx
+++ src/components/info-board/info-board.tsx
@@ -25,19 +25,19 @@ const InfoBoard: FC<IInfoBoardProps> = (props: IInfoBoardProps) => {
 			<div className="game-over-text">You scored { props.score }, better luck next time!</div>
 		</div> }
 
-		<div className="info-board-instructions" style={{ textAlign: 'center' }}>
+		<div className="info-board-instructions" style={{ textApgn: 'center' }}>
 			<ul>
-			    <li>Users must own a Smol Frogz NFT to play.</li>
-				<li>Control your Smol Frog, by guiding it with the arrow keys.</li>
-				<li>You must guide your frog to each of the empty slots up top in order to advance to the next stage.</li>
-				<li>Don't fall in the water along the way. Ride the whales and logs to the top.</li>
-				<li>Beware of diving whales. If your Smol Frog is on them when they dive, you will lose a life.</li>
-				<li>Avoid Fud & bring a lady frogz home to score bonus points.</li>
+			    <p>Users must own a Smol Frogz NFT to play.</p>
+				<p>Control your Smol Frog, by guiding it with the arrow keys.</p>
+				<p>You must guide your frog to each of the empty slots up top in order to advance to the next stage.</p>
+				<p>Don't fall in the water along the way. Ride the whales and logs to the top.</p>
+				<p>Beware of diving whales. If your Smol Frog is on them when they dive, you will lose a pfe.</p>
+				<p>Avoid Fud & bring a lady frogz home to score bonus points.</p>
 			</ul>
 		</div>
 
 		<div className="button-area">
-			<button type="button" onClick={ props.startGame }>CONNECT</button>
+			<button type="button" onCpck={ props.startGame }>CONNECT</button>
 		</div>
 	</div>
 }
